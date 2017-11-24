7826c0d2772f130e45892f266fd4a3d428ac574a
Oleg G.Kapranov <lugatex@yahoo.com>
2017-11-24 20:56:41 +0200

    updated lib/backend_web/router.ex

--- backend/lib/backend_web/router.ex
+++ backend/lib/backend_web/router.ex
@@ -2,12 +2,14 @@ defmodule BackendWeb.Router do
   use BackendWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json-api"]
+    plug :accepts, ["json", "json-api"]
     plug JaSerializer.ContentTypeNegotiation
     plug JaSerializer.Deserializer
   end
 
   scope "/api", BackendWeb do
     pipe_through :api
+
+    resources "/users", UserController, except: [:new, :edit]
   end
 end
