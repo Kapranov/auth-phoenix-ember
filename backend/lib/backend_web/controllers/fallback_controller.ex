15742e3cd8b61a90e762ba53ec05b17c3c47ef3b
Oleg G.Kapranov <lugatex@yahoo.com>
2017-11-24 20:52:10 +0200

    added lib/backend_web/controllers/fallback_controller.ex

---
+++ backend/lib/backend_web/controllers/fallback_controller.ex
@@ -0,0 +1,15 @@
+defmodule BackendWeb.FallbackController do
+  use BackendWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> render(BackendWeb.ChangesetView, "error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> render(BackendWeb.ErrorView, :"404")
+  end
+end
